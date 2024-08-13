20a27aacf7c1efbb5647ea9dd57d2f8c2a964f09
leocarten <lcarten14@gmail.com>
2024-08-13 14:28:07 -0400

    Add project files.

---
+++ WordleAnswerGUI/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace WordleAnswerGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dateTimePicker = new DateTimePicker();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // dateTimePicker
+            // 
+            dateTimePicker.Location = new Point(12, 65);
+            dateTimePicker.Name = "dateTimePicker";
+            dateTimePicker.Size = new Size(281, 27);
+            dateTimePicker.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(85, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Select Date";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(dateTimePicker);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DateTimePicker dateTimePicker;
+        private Label label1;
+    }
+}
