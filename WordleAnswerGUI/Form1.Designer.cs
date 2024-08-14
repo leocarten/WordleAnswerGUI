d2f7e06e3003430cf2c4a0f2c70be719526c5158
leocarten <lcarten14@gmail.com>
2024-08-14 11:05:43 -0400

    Non UI thread blocking desktop application that gets the wordle of the day for past and future.

--- WordleAnswerGUI/Form1.Designer.cs
+++ WordleAnswerGUI/Form1.Designer.cs
@@ -28,41 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
-            dateTimePicker = new DateTimePicker();
             label1 = new Label();
+            dateTimePicker1 = new DateTimePicker();
             button1 = new Button();
             SuspendLayout();
-            // 
-            // dateTimePicker
-            // 
-            dateTimePicker.CalendarForeColor = SystemColors.ButtonHighlight;
-            dateTimePicker.CalendarMonthBackground = SystemColors.InfoText;
-            dateTimePicker.CalendarTitleBackColor = Color.Salmon;
-            dateTimePicker.Location = new Point(15, 67);
-            dateTimePicker.Name = "dateTimePicker";
-            dateTimePicker.Size = new Size(281, 27);
-            dateTimePicker.TabIndex = 0;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(15, 34);
+            label1.Location = new Point(12, 9);
             label1.Name = "label1";
             label1.Size = new Size(85, 20);
-            label1.TabIndex = 1;
+            label1.TabIndex = 0;
             label1.Text = "Select Date";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(12, 32);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(273, 27);
+            dateTimePicker1.TabIndex = 1;
             // 
             // button1
             // 
-            button1.BackColor = Color.SaddleBrown;
+            button1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button1.BackColor = Color.Sienna;
             button1.ForeColor = SystemColors.ButtonHighlight;
-            button1.Location = new Point(109, 176);
+            button1.Location = new Point(108, 159);
             button1.Name = "button1";
-            button1.Size = new Size(267, 29);
+            button1.Size = new Size(310, 29);
             button1.TabIndex = 2;
-            button1.Text = "Get Wordle Answer!";
+            button1.Text = "Get Wordle Answer";
             button1.UseVisualStyleBackColor = false;
-            button1.Click += button1_Click;
+            button1.Click += button1_Click_1;
             // 
             // Form1
             // 
@@ -71,8 +69,8 @@
             BackColor = Color.SandyBrown;
             ClientSize = new Size(504, 306);
             Controls.Add(button1);
+            Controls.Add(dateTimePicker1);
             Controls.Add(label1);
-            Controls.Add(dateTimePicker);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
@@ -82,8 +80,8 @@
 
         #endregion
 
-        private DateTimePicker dateTimePicker;
         private Label label1;
+        private DateTimePicker dateTimePicker1;
         private Button button1;
     }
 }
--- WordleAnswerGUI/Form1.cs
+++ WordleAnswerGUI/Form1.cs
@@ -1,3 +1,6 @@
+using System.Net.Http;
+using System.Text.Json;
+
 namespace WordleAnswerGUI
 {
     public partial class Form1 : Form
@@ -22,5 +25,130 @@ namespace WordleAnswerGUI
             //    }
             //    MessageBox.Show(str);
         }
-}
+
+        private string mapMonthToNum(string month) 
+        {
+            if (month.Contains("January"))
+            {
+                return "01";
+            }
+            else if (month.Contains("February"))
+            {
+                return "02";
+            }
+            else if (month.Contains("March"))
+            {
+                return "03";
+            }
+            else if (month.Contains("April"))
+            {
+                return "04";
+            }
+            else if (month.Contains("May"))
+            {
+                return "05";
+            }
+            else if (month.Contains("June"))
+            {
+                return "06";
+            }
+            else if (month.Contains("July"))
+            {
+                return "07";
+            }
+            else if (month.Contains("August"))
+            {
+                return "08";
+            }
+            else if (month.Contains("September"))
+            {
+                return "09";
+            }
+            else if (month.Contains("October"))
+            {
+                return "10";
+            }
+            else if (month.Contains("November"))
+            {
+                return "11";
+            }
+            else if (month.Contains("December"))
+            {
+                return "12";
+            }
+            else
+            {
+                return "0";
+            }
+        }
+
+        private async Task<string> MakeRequest(string year, string month, string day) 
+        {            
+            string url = $"https://www.nytimes.com/svc/wordle/v2/{year}-{month}-{day}.json";
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+
+                    response.EnsureSuccessStatusCode();
+
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    //await Task.Delay(5000); // simulate network latency lol
+
+                    using (JsonDocument doc = JsonDocument.Parse(jsonResponse)) // parse the json
+                    {
+                        if (doc.RootElement.TryGetProperty("solution", out JsonElement solutionElement))
+                        {
+                            return solutionElement.GetString();
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+
+                    return jsonResponse;
+                }
+                catch (HttpRequestException e)
+                {
+                    MessageBox.Show("There was an error trying to get the data you requested.");
+                    return null;
+                }
+            }
+        }
+
+
+        private async void button1_Click_1(object sender, EventArgs e)
+        {
+            string dateChosen = dateTimePicker1.Text;
+            string[] arr = dateChosen.Split(',');
+
+            // format month
+            string month = arr[1];
+            string monthToInt = mapMonthToNum(month);
+
+            // format year
+            string year = arr[2];
+            year = year.Trim(); //remove any additional white space
+
+            // format day
+            int length_of_second_item = arr[1].Length;
+            char first_char_of_day = arr[1][length_of_second_item - 2];
+            char second_char_of_day = arr[1][length_of_second_item - 1];
+            if(first_char_of_day == ' ' || first_char_of_day == null)
+            {
+                first_char_of_day = '0';
+            }
+            string day = first_char_of_day.ToString();
+            day += second_char_of_day;
+
+            // now we need to make the request using a TASK
+            string ans = await MakeRequest(year, monthToInt, day); // do this without blocking the UI thread!! window is still responsive
+            if (ans != null) // make sure the request was successful
+            {
+                MessageBox.Show($"The wordle of the day on {dateChosen} is: {ans}");
+            }
+        }
+    }
 }
