898b1eb1e73cbbcdc000b882d1d56844b6398821
leocarten <lcarten14@gmail.com>
2024-08-14 21:12:18 -0400

    Green Interface (v2)

--- WordleAnswerGUI/Form1.Designer.cs
+++ WordleAnswerGUI/Form1.Designer.cs
@@ -31,12 +31,14 @@
             label1 = new Label();
             dateTimePicker1 = new DateTimePicker();
             button1 = new Button();
+            label2 = new Label();
+            label3 = new Label();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 9);
+            label1.Location = new Point(12, 96);
             label1.Name = "label1";
             label1.Size = new Size(85, 20);
             label1.TabIndex = 0;
@@ -44,7 +46,7 @@
             // 
             // dateTimePicker1
             // 
-            dateTimePicker1.Location = new Point(12, 32);
+            dateTimePicker1.Location = new Point(12, 119);
             dateTimePicker1.Name = "dateTimePicker1";
             dateTimePicker1.Size = new Size(273, 27);
             dateTimePicker1.TabIndex = 1;
@@ -52,22 +54,46 @@
             // button1
             // 
             button1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            button1.BackColor = Color.Sienna;
-            button1.ForeColor = SystemColors.ButtonHighlight;
-            button1.Location = new Point(108, 159);
+            button1.BackColor = Color.DarkGreen;
+            button1.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            button1.ForeColor = SystemColors.ButtonFace;
+            button1.Location = new Point(146, 197);
             button1.Name = "button1";
-            button1.Size = new Size(310, 29);
+            button1.Size = new Size(340, 40);
             button1.TabIndex = 2;
             button1.Text = "Get Wordle Answer";
             button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click_1;
             // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Showcard Gothic", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.DarkGreen;
+            label2.Location = new Point(177, 37);
+            label2.Name = "label2";
+            label2.Size = new Size(260, 35);
+            label2.TabIndex = 3;
+            label2.Text = "WORDLE GUESSER";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 8F);
+            label3.Location = new Point(12, 299);
+            label3.Name = "label3";
+            label3.Size = new Size(98, 19);
+            label3.TabIndex = 4;
+            label3.Text = "By: Leo Carten";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.SandyBrown;
-            ClientSize = new Size(504, 306);
+            BackColor = Color.LightGreen;
+            ClientSize = new Size(592, 327);
+            Controls.Add(label3);
+            Controls.Add(label2);
             Controls.Add(button1);
             Controls.Add(dateTimePicker1);
             Controls.Add(label1);
@@ -84,5 +110,7 @@
         private Label label1;
         private DateTimePicker dateTimePicker1;
         private Button button1;
+        private Label label2;
+        private Label label3;
     }
 }
